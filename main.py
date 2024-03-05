63cfa3f36cb32514811d05880dfc7e0650436034
Soufiane Aatab <aatab.soufiane@gmail.com>
2024-03-05 14:46:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ register_diff_op('max_pool2d', max_pool2d_diff_op)
 register_diff_op('nll_loss', nll_loss)
 register_diff_op('log_softmax', log_softmax_diff)
 
-from examples.sine import sine_example
+# from examples.sine import sine_example
 from examples.conv import conv_example
 
 conv_example()
